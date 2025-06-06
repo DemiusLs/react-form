c7c65a7cf52d6ec41452b4d6abe0354a8087e4b8
DemiusLs <dloss320@gmail.com>
2025-06-06 18:54:57 +0200

    feat: milestone 2 done

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,87 @@ import blogPosts from './data/posts'
 
 function App() {
 
+  const [inputValue, setInputValue] = useState("qui")
+  const [initialPosts, setInitialPost] = useState(blogPosts)
+
+
+  const handleSubmit = (event) => {
+
+    event.preventDefault();
+    if (inputValue.trim() !== "") {
+      const newPost = { titolo: inputValue, id: initialPosts.length + 1, contenuto: "giusto due parole" }
+
+      const newPostsArr = [...initialPosts, newPost]
+
+      setInitialPost(newPostsArr)
+    }
+
+
+  };
+
+  const deletePost = (postId) => {
+    console.log(postId);
+
+    const newPostsArr = initialPosts.filter((curPost) => curPost.id !== postId)
+    console.log(newPostsArr)
+    setInitialPost(newPostsArr)
+  }
+
 
   return (
     <>
       <div className="container">
 
-        <h1>Lista articoli</h1>
+        <h1 className='pt-5 pb-3'>Lista articoli</h1>
 
-        <ul className='list-group'>
-          {blogPosts.map((curPost) =>
+        <ul className='list-group '>
+          {initialPosts.map((curPost) =>
 
-            (<li className='list-group-item' key={curPost.id}>{curPost.titolo}</li>)
+          (<li className='list-group-item' key={curPost.id}>{curPost.titolo}
+            <button
+              onClick={() => {
+                deletePost(curPost.id)
+              }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-trash"
+                viewBox="0 0 16 16"
+              >
+                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+                <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />Add commentMore actions
+              </svg>
+            </button>
+
+            <button>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
+              </svg>
+            </button>
+          </li>)
           )}
-        </ul>
-      </div>
 
+
+        </ul>
+
+
+        <form action="" className='d-flex flex-column py-5' onSubmit={handleSubmit}>
+
+          <label htmlFor="newArticle" className="form-label">Inserisci un nuovo articolo</label>
+          <div className='d-flex  justify-content-between'>
+            <input type="text" className="form-control" id="newArticle" value={inputValue} autoComplete='off' onChange={event => (setInputValue(event.target.value)
+            )} />
+
+            <button type='submit' className='btn btn-primary'>Aggiungi</button>
+
+          </div>
+
+
+        </form>
+      </div>
 
     </>
   )
