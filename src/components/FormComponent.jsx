9a1b598857d5c2d57fbf8b1571a02f7bb7040388
DemiusLs <dloss320@gmail.com>
2025-06-07 11:55:57 +0200

    feat: modify listitem init

---
+++ src/components/FormComponent.jsx
@@ -0,0 +1,20 @@
+const FormComponent = ({ handleSubmit, setInputValue ,inputValue }) => {
+
+    return (
+        <form action="" className='d-flex flex-column py-5' onSubmit={handleSubmit}>
+
+            <label htmlFor="newArticle" className="form-label">Inserisci un nuovo articolo</label>
+            <div className='d-flex  justify-content-between'>
+                <input type="text" className="form-control" id="newArticle" value={inputValue} autoComplete='off' onChange={event => (setInputValue(event.target.value)
+                )} />
+
+                <button type='submit' className='btn btn-primary'>Aggiungi</button>
+
+            </div>
+
+
+        </form>
+    )
+}
+
+export default FormComponent;
